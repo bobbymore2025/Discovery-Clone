a0054b541f5b75fae876d0b625b69ce3c7aa85e4
Priya_darshini_s <priya06200@gmail.com>
2022-01-23 02:29:40 +0530

    automatic and manual carousel added, updated shorts page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css';
 import { Premium } from './components/premium';
 import { Navbar } from './components/navbar';
 import { Explore } from './components/explore';
+import { Shorts } from './components/shorts';
 
 
 function App() {
   return (
     <div className="App">
-    <Premium />
+    <Explore />
     </div>
   );
 }
